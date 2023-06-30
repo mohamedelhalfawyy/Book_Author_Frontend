b26131b1f137ec9d148dce94f2e97e87227e4f6b
Mohamed Halfawy <mohamed.elhalfawyy@gmail.com>
2023-06-30 17:01:53 +0300

    Add the Footer for the pages and the css for it

---
+++ src/Components/Common/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../CSS/Footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h1>DigitalShelf</h1>
+        </div>
+    );
+}
+
+export default Footer;
