887823f498560dc8ac609b738ccd375c62375b21
Mohamed Halfawy <mohamed.elhalfawyy@gmail.com>
2023-06-30 17:28:59 +0300

    Adjust the index.js and app.js for pointing to the right routes and right files & change website logo and title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {Navigate, Route, Routes} from "react-router-dom";
+import BooksList from "./Components/BooksList";
+import Registration from "./Components/Registration";
+import Login from "./Components/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const isLoggedIn = !!localStorage.getItem("token"); // Check if user is logged in
+
+    return (
+      <div>
+        <main>
+          <Routes>
+            <Route path="/" exact element={<Navigate to={isLoggedIn ? "/books" : "/login"} />}/>
+            <Route path="/books" element={<BooksList/>}/>
+            <Route path="/registration" element={<Registration/>}/>
+            <Route path="/login" element={<Login/>}/>
+          </Routes>
+        </main>
+      </div>
+  )
 }
 
 export default App;
